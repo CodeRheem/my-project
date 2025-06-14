c9d27abbcc9cbfefddf35865f9d564b316039a80
Ibraheem Abdul-rahman <ibraheemabdulrahman305@gmail.com>
2025-06-14 10:19:47 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ErrorBoundary>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/todos/:id" element={<TodoDetails />} />
+          <Route path="/todos/:id" element={<ToDoDetails />} />
           <Route path="/error" element={<ErrorTester />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
